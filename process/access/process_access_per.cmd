6e8057b8cde30b7b97b45d9c0b64b2357b476afd
Craig Arthur <wcarthur@gmail.com>
2021-06-24 07:49:43 +1000

    Add Perth and Adelaide wind gust processing

---
+++ process/access/process_access_per.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM Process ACCESS-PH data 
+CALL conda.bat activate base
+set PYTHONPATH=C:\Workspace\lib\python
+python C:\workspace\bin\process\access\process_windgust.py -c C:\workspace\bin\process\access\process_windgust_per.ini -v
