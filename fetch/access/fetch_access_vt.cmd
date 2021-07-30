07493cf1116bf02479f459a54f079937c5b5051c
Craig Arthur <wcarthur@gmail.com>
2021-07-30 14:24:37 +1000

    Separate fetch for each domain

---
+++ fetch/access/fetch_access_vt.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM Fetch ACCESS-C3 from the BoM using FTP client and script
+CALL conda.bat activate process
+set PYTHONPATH=C:\Workspace\lib\python
+cd \incoming\que
+python C:\workspace\lib\python\sftpscriptrunner.py -c C:\workspace\bin\fetch\access\fetch_access_vt.ini -v
