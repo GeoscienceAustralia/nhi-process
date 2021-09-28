b0f4e5a988e094c77e8fa7a7a6173a45cd0c9aec
Craig Arthur <wcarthur@gmail.com>
2021-09-29 08:50:13 +1000

    Add Darwin processing config files

---
+++ process/access/process_access_dar.cmd
@@ -0,0 +1,7 @@
+@echo off
+REM Process ACCESS-DN data
+CALL conda.bat activate process
+set PYTHONPATH=C:\Workspace\lib\python
+
+python C:\workspace\bin\process\access\process_windgust.py -c C:\workspace\bin\process\access\process_windgust_dar.ini -v
+python C:\workspace\bin\process\access\process_helicity.py -c C:\workspace\bin\process\access\process_helicity_dar.ini -v
